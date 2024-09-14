0d96ff7e4ee124108f494cb033d5bd0cd1a74699
Ubuntu <kit214@lab9ESB75.vvfyilsrawwevfva4hjuedeo3h.px.internal.cloudapp.net>
2024-09-14 13:12:36 +0000

    icon discord added

--- view/DiscordLogin.php
+++ view/DiscordLogin.php
@@ -14,11 +14,21 @@ class DiscordLogin
 </head>
 <body>
 <div class='container'>
-<p>Please press the Link to go to discord page</p>
+  <div class="container py-5 h-100">
+  <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+      <div class="card shadow-2-strong bg-light" style="border-radius: 1rem;">
+        <div class="card-body p-5 text-center">
+<button class="btn btn-primary" type="button"><i class="bi bi-discord"></i></button>
+
 <a href="https://discord.com/oauth2/authorize?client_id=1283049993837744129&response_type=code&redirect_uri=https%3A%2F%2Flab-d00a6b41-7f81-4587-a3ab-fa25e5f6d9cf.australiaeast.cloudapp.azure.com%3A7107%2FAssignment%2Findex.php%3Faction%3DdiscordInfo&scope=identify+guilds">
-Login Discord</a>
-                               
+Discord Login</a>
 </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
 </body>
 </html>
 <?php
